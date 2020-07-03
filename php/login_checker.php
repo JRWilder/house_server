187dbe929f0293373302909c9a3209bab72723b6
Francis Scotland jr <francisscotlandjr@Franciss-MacBook-Pro.local>
2020-07-02 20:06:48 -0400

    added SESSION tokens

--- php/login_checker.php
+++ php/login_checker.php
@@ -1,7 +1,8 @@
 <?php
 	$username = $_POST["username"];
 	$password = $_POST["password"];
-	echo $username.' '.$password;
+
+
 	$connection = new mysqli("localhost","root","root","house_server");
 
 	if($connection->connect_error){
@@ -12,13 +13,15 @@
 		$result = $connection->query("SELECT * FROM users");
 		while($user = $result->fetch_assoc()){
 			if( ($user['username'] == $username) and ($user['password'] == $password )){
-				#header('Location: ../device/device_list.php');
-				echo 'Welcome '.$user['first'].' '.$user['last'].'!';
+				session_start();
+				$_SESSION['name'] = $user['name'];
+				#echo $_SESSION['name'];
+				header('Location: ../html/home.html');
+				#echo 'Welcome '.$user['first'].' '.$user['last'].'!';
 				die();
 			}
 		}
 	}
-	echo "Nope";
-	#header('Location: fail_to_login.html');
+	header('Location: ../html/login_fail.html');
 
 ?>
